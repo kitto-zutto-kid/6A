e606b6a3c781b24c675997b955b6a6c21dbb2c4d
あちゃぴ <67042875+achapi@users.noreply.github.com>
2024-09-01 20:33:14 +0900

    更新

--- js/koyagi.js
+++ js/koyagi.js
@@ -1,17 +1,17 @@
 document.addEventListener("DOMContentLoaded", function() {
 	const partsData = {
-		"体色": Array.from({ length: 20 }, (_, i) => `${i + 1}.PNG`),
-		"目": Array.from({ length: 11 }, (_, i) => `${i + 1}.PNG`),
-		"眉": Array.from({ length: 4 }, (_, i) => `${i + 1}.PNG`),
-		"耳": Array.from({ length: 40 }, (_, i) => `${i + 1}.PNG`),
-		"口": Array.from({ length: 7 }, (_, i) => `${i + 1}.PNG`),
-		"ほっぺ": Array.from({ length: 4 }, (_, i) => `${i + 1}.PNG`),
-		"角": Array.from({ length: 7 }, (_, i) => `${i + 1}.PNG`),
-		"ヒヅメ": Array.from({ length: 4 }, (_, i) => `${i + 1}.PNG`),
-		"しっぽ": Array.from({ length: 40 }, (_, i) => `${i + 1}.PNG`),
-		"首輪": Array.from({ length: 6 }, (_, i) => `${i + 1}.PNG`),
-		"アクセサリー": Array.from({ length: 3 }, (_, i) => `${i + 1}.PNG`),
-		"体線画": ["1.PNG"]
+		"taishoku": Array.from({ length: 20 }, (_, i) => `${i + 1}.PNG`),
+		"me": Array.from({ length: 11 }, (_, i) => `${i + 1}.PNG`),
+		"mayu": Array.from({ length: 4 }, (_, i) => `${i + 1}.PNG`),
+		"mimi": Array.from({ length: 40 }, (_, i) => `${i + 1}.PNG`),
+		"kuchi": Array.from({ length: 7 }, (_, i) => `${i + 1}.PNG`),
+		"hoho": Array.from({ length: 4 }, (_, i) => `${i + 1}.PNG`),
+		"tsuno": Array.from({ length: 7 }, (_, i) => `${i + 1}.PNG`),
+		"hidume": Array.from({ length: 4 }, (_, i) => `${i + 1}.PNG`),
+		"sippo": Array.from({ length: 40 }, (_, i) => `${i + 1}.PNG`),
+		"kubiwa": Array.from({ length: 6 }, (_, i) => `${i + 1}.PNG`),
+		"accessory": Array.from({ length: 3 }, (_, i) => `${i + 1}.PNG`),
+		"senga": ["1.PNG"]
 	};
 
 	const partSelector = document.getElementById("partSelector");
@@ -20,14 +20,26 @@ document.addEventListener("DOMContentLoaded", function() {
 	function generatePartSelector() {
 		const partCategoryContainer = document.createElement('div');
 		partCategoryContainer.className = 'part-category-container';
-
+		const name = {
+			"taishoku": "毛の色",
+			"me": "目",
+			"mayu": "眉",
+			"mimi": "耳",
+			"kuchi": "口",
+			"hoho": "ほっぺ",
+			"tsuno": "ツノ",
+			"hidume": "ヒヅメ",
+			"sippo": "しっぽ",
+			"kubiwa": "kubiwa",
+			"accessory": "アクセサリー",
+		};
 		Object.keys(partsData).forEach((category, index) => {
-			if (category === "体線画") return; // Skip "体線画"
+			if (category === "senga") return; // Skip "senga"
 
 			const partCategory = document.createElement('div');
 			partCategory.className = 'part-category';
 			partCategory.dataset.part = category;
-			partCategory.textContent = category;
+			partCategory.textContent = name[category];
 
 			partCategory.addEventListener('click', function() {
 				displayPartOptions(category);
@@ -84,17 +96,17 @@ document.addEventListener("DOMContentLoaded", function() {
 	function updatePreview(category, file) {
 		// カテゴリーごとの z-index を定義
 		const zIndexMap = {
-			"体色": 1,
-			"目": 2,
-			"眉": 3,
-			"耳": 4,
-			"口": 5,
-			"ほっぺ": 6,
-			"角": 7,
-			"ヒヅメ": 8,
-			"しっぽ": 9,
-			"首輪": 10,
-			"アクセサリー": 11
+			"taishoku": 1,
+			"me": 2,
+			"mayu": 3,
+			"mimi": 4,
+			"kuchi": 5,
+			"hoho": 6,
+			"tsuno": 7,
+			"hidume": 8,
+			"sippo": 9,
+			"kubiwa": 10,
+			"accessory": 11
 		};
 	
 		// Get existing images from the preview
@@ -120,34 +132,34 @@ document.addEventListener("DOMContentLoaded", function() {
 	// Generate the part selector UI
 	generatePartSelector();
 
-	// Set default image for 体線画
-	updatePreview("体線画", "1.PNG");
+	// Set default image for senga
+	updatePreview("senga", "1.PNG");
 
 	// Open all part options
-	displayPartOptions("体色");
-	displayPartOptions("目");
-	displayPartOptions("眉");
-	displayPartOptions("耳");
-	displayPartOptions("口");
-	displayPartOptions("ほっぺ");
-	displayPartOptions("角");
-	displayPartOptions("ヒヅメ");
-	displayPartOptions("しっぽ");
-	displayPartOptions("首輪");
-	displayPartOptions("アクセサリー");
-	displayPartOptions("体色");
+	displayPartOptions("taishoku");
+	displayPartOptions("me");
+	displayPartOptions("mayu");
+	displayPartOptions("mimi");
+	displayPartOptions("kuchi");
+	displayPartOptions("hoho");
+	displayPartOptions("tsuno");
+	displayPartOptions("hidume");
+	displayPartOptions("sippo");
+	displayPartOptions("kubiwa");
+	displayPartOptions("accessory");
+	displayPartOptions("taishoku");
 
-	updatePreview("体色", "1.PNG");
-	updatePreview("目", "1.PNG");
-	updatePreview("眉", "1.PNG");
-	updatePreview("耳", "1.PNG");
-	updatePreview("口", "1.PNG");
-	updatePreview("ほっぺ", "1.PNG");
-	updatePreview("角", "1.PNG");
-	updatePreview("ヒヅメ", "1.PNG");
-	updatePreview("しっぽ", "1.PNG");
-	updatePreview("首輪", "1.PNG");
-	updatePreview("アクセサリー", "1.PNG");
+	updatePreview("taishoku", "1.PNG");
+	updatePreview("me", "1.PNG");
+	updatePreview("mayu", "1.PNG");
+	updatePreview("mimi", "1.PNG");
+	updatePreview("kuchi", "1.PNG");
+	updatePreview("hoho", "1.PNG");
+	updatePreview("tsuno", "1.PNG");
+	updatePreview("hidume", "1.PNG");
+	updatePreview("sippo", "1.PNG");
+	updatePreview("kubiwa", "1.PNG");
+	updatePreview("accessory", "1.PNG");
 
 	// Save button functionality
 	document.getElementById("saveButton").addEventListener("click", function() {
