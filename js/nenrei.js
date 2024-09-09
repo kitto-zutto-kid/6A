bd5174d1bc8d9ed93b5a04e2be679a828934dbdd
あちゃぴ <67042875+achapi@users.noreply.github.com>
2024-09-09 23:18:40 +0900

    Update nenrei.js

--- js/nenrei.js
+++ js/nenrei.js
@@ -17,14 +17,21 @@ const question = [
 	[`遅刻は多い方だ`, -10, 5],
 	[`よく子供っぽいと言われる`, -10, 5],
 	[`ゲームは好きですか？`, -10, 0],
-	[`電車が好きですか？`, -10000000000000, 10]
+	[`サンタさんはいると思いますか？`, 10, -10000],
+	[`3×3は9ですか？`, -10, 100000],
+	[`蛍光色は好きですか？`, 0, -1000],
+	[`「はい」を押してください`, 10, -1000000],
+	[`料理できますか？`, 1000, 0],
+	[`パズルは得意ですか？`, -100, 50],
+	[`単純作業が好きですか？`, 0, -10],
+	[`この診断は楽しかったですか？`, 20, -1000000000]
 ];
 let sum = 0;
 let now = 0;
 document.getElementById("question").innerHTML = question[now][0];
 function next(){
 	if (now + 1 == question.length){
-		document.getElementById("result").innerHTML = `${sum}歳`;
+		document.getElementById("result").innerHTML = `あなたの精神年齢は${sum}歳です`;
 		return;
 	}
 	now++;
